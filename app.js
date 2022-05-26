2a132795d3405b9c7b81420daf92e12f9c913fa0
Jerónimo Da Cunha <105886027+jeronimodacunhanunga@users.noreply.github.com>
2022-05-26 22:27:08 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,17 +1,71 @@
-let menuToggle = document.querySelector('.toggle');
-let navigation = document.querySelector('.navigation')
-menuToggle.onclick = function(){
-  menuToggle.classList.toggle('active');
-  navigation.classList.toggle('active');
-}
-// add active class in selected list item
-let list = document.querySelectorAll('.list');
-for (let i = 0; i < list.length; i++) {
-  list[i].onclick = function() {
-    let j = 0;
-    while(j < list.length) {
-      list[j++].className = 'list';
+
+let btn = document.querySelectorAll('#track #play_pause');
+let song = document.querySelectorAll('#music');
+
+let song_index = 0;
+let song_status = 0;
+let index_no;
+
+btn.forEach((btn,index) => {
+  btn.addEventListener('click', function(){
+
+  	if (song_index == 1) {
+  		document.querySelector(".active_song").pause();
+  		document.querySelector(".active_song").classList.remove("active_song");
+  		document.querySelector(".active_btn").innerHTML = "<i class='bx bx-play'></i>";
+  		document.querySelector(".active_btn").classList.remove("active_btn");
+  	}else{
+        song_index = 1;
+  	}
+  	
+  	song[index].classList.add("active_song");
+    this.classList.add("active_btn");
+
+  	song[index].currentTime = 0;
+
+  	if (song_status == 0) {
+  		song[index].play();
+  		song_status = 1;
+  		setInterval(update_second, 1000);
+  		index_no = index;
+  		this.innerHTML = "<i class='bx bx-pause'></i>";
+  	}else{
+  		song[index].pause();
+  		this.innerHTML = "<i class='bx bx-play'></i>";
+  		song_status = 0;
+  	}
+
+  });
+});
+
+/*This function will be execute in Every Second*/
+ function update_second(){
+	if (song[index_no].ended) {
+      btn[index_no].innerHTML = "<i class='bx bx-play'></i>";
+      clearInterval(update_second);
     }
-    list[i].className = 'list active';
-  }
+ }
+
+
+/*side menu*/
+function menu_action(){
+	let side_nav = document.querySelector('.side_menu');
+    side_nav.classList.toggle("side_nav_active");
+}
+
+/*main menu*/
+function open_close_menu(){
+	let nav_content = document.querySelector('.nav_content');
+    nav_content.classList.toggle("active_top_navbar");
+}
+
+
+
+// Function execute When the user scrolls the page
+window.onscroll = () =>{
+
+  var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+  var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+  var scrolled = (winScroll / height) * 100;
+  document.getElementById("Bar").style.width = scrolled + "%";
 }
